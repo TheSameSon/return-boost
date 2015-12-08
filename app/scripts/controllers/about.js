9d1f8bd304719affb3f3110c30ee0ebf60cb43c9
Maxim Semenenkov <maxim.semenenkov@gmail.com>
2015-12-08 17:34:00 +0200

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name strategiesAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the strategiesAppApp
+ */
+angular.module('strategiesAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name strategiesAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the strategiesAppApp
+ */
+angular.module('strategiesAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
